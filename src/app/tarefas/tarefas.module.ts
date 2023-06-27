a00e9950365571834ac3afcbf43de28aa7fed891
rigorjs <igorga.jsantos@gmail.com>
2023-06-27 17:57:51 -0300

    generate routes

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar/listar-tarefa.component';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
